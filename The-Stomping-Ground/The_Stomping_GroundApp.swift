582621235580f972c4111cee107cb05745f57dbe
Chandan Brown <chandanbrown@yahoo.com>
2022-12-16 22:49:09 -0500

    Initial Commit

---
+++ The-Stomping-Ground/The_Stomping_GroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Stomping_GroundApp.swift
+//  The-Stomping-Ground
+//
+//  Created by Chandan Brown on 12/16/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Stomping_GroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
